16ba30b1f3746dc6b5fe1807deaba9db46a6b8e4
Helton Ribeiro <oiheltong@gmail.com>
2026-02-07 02:15:50 -0300

    Initial commit

---
+++ assets/JS/scrip.js
@@ -0,0 +1,68 @@
+//  CONFIGS
+document.querySelectorAll('.imageTrabalho video').forEach(video => {
+    video.addEventListener('mouseenter', () => {
+        video.play();
+    });
+
+    video.addEventListener('mouseleave', () => {
+        video.pause();
+        video.currentTime = 0;
+    });
+});
+
+const linksNav = document.querySelectorAll('nav a');
+
+linksNav.forEach(link => {
+    link.addEventListener('click', function (e) {
+        // 1. Impede o comportamento padrão (mudar a URL)
+        e.preventDefault();
+
+        // 2. Pega o ID do destino (ex: #Soluções)
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        // 3. Verifica se o elemento existe e faz a rolagem
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 80, // O -80 é para compensar a altura do seu Header fixo
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+//   
+
+gsap.registerPlugin(ScrollTrigger);
+
+//  Variaveis 
+const tl = gsap.timeline();
+
+tl.from("header", {
+    opacity:0,
+    y:-20,
+    duration: .8
+})
+
+tl.from(".textAnimado", {
+    opacity: 0,
+    duration:.8
+})
+
+tl.from(".status", {
+    opacity: 0,
+    filter:"blur(10px)",
+    y: -20,
+    duration:.8
+})
+
+const textoAnimado = document.querySelectorAll(".textAnimado");
+
+const splitType = new SplitType(textoAnimado, {type: "chars"})
+
+gsap.from(splitType.chars, {
+    opacity: 0,
+    filter:"blur(3px)",
+    stagger: 0.1
+})
