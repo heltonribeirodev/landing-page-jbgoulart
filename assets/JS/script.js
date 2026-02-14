897908c08492e2ebfb22458c153064102128a787
Helton Ribeiro <oiheltong@gmail.com>
2026-02-14 17:37:45 -0300

    Animations Update

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,11 @@
+//  Menu Hamburguer
 const btnMenu = document.getElementById("btnMenu");
 const body = document.body;
 
 function toggleMenu() {
     const nav = document.getElementById("nav");
     const icon = btnMenu.querySelector("i"); // Seleciona o ícone dentro do botão
-    
+
     const isMenuOpen = nav.classList.toggle('active');
 
     if (isMenuOpen) {
@@ -20,15 +21,43 @@ function toggleMenu() {
 
 btnMenu.addEventListener('click', toggleMenu);
 
-// Garantir que o ícone volte ao normal se clicar em um link
 const menuLinks = document.querySelectorAll('#menu a');
 menuLinks.forEach(link => {
     link.addEventListener('click', () => {
         const nav = document.getElementById("nav");
         const icon = btnMenu.querySelector("i");
-        
+
         nav.classList.remove('active');
         body.style.overflow = 'auto';
         icon.classList.replace('bi-x-lg', 'bi-list');
     });
+});
+
+// Otimização video
+document.addEventListener("DOMContentLoaded", function () {
+    var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+
+    if ("IntersectionObserver" in window) {
+        var lazyVideoObserver = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (video) {
+                if (video.isIntersecting) {
+                    // Quando o vídeo aparece na tela, troca data-src por src
+                    for (var source in video.target.children) {
+                        var videoSource = video.target.children[source];
+                        if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                            videoSource.src = videoSource.dataset.src;
+                        }
+                    }
+
+                    video.target.load(); // Carrega o vídeo
+                    video.target.classList.remove("lazy");
+                    lazyVideoObserver.unobserve(video.target); // Para de observar este vídeo
+                }
+            });
+        });
+
+        lazyVideos.forEach(function (lazyVideo) {
+            lazyVideoObserver.observe(lazyVideo);
+        });
+    }
 });
