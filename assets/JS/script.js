feedda01c1809cdac750a100da775bddc65876d1
Helton Ribeiro <oiheltong@gmail.com>
2026-02-14 19:20:52 -0300

    NAV sem afetar URL

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -61,3 +61,24 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+// BOTÕES NAV SEM MUDAR URL
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); //
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            const headerOffset = 80; 
+            const elementPosition = targetElement.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
